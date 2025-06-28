ebca743ac9227ccd629acab3f89cd7c005d12b17
Chaitany233patil <patilchaitany529@gmail.com>
2025-06-28 16:35:42 +0530

    .

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { Hero } from "./components/Hero";
 
 export default function Home() {
   return (
-    <div className="bg-[#333348]">
+    <div>
       <Hero />
       <Features />
       <Contact />
